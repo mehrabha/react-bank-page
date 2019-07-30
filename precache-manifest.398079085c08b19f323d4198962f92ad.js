6b75769b089f37ccb039448461e5daee7c7a4c0b
mehrabha <mehrabhfz@gmail.com>
2019-07-30 13:57:54 -0400

    Updates

---
+++ precache-manifest.398079085c08b19f323d4198962f92ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3cee157c313231a3449ebf72475f06",
+    "url": "/react-bank-page/index.html"
+  },
+  {
+    "revision": "120a42163d6b926ad59c",
+    "url": "/react-bank-page/static/css/main.0f13161b.chunk.css"
+  },
+  {
+    "revision": "d6e4699995db7e154c36",
+    "url": "/react-bank-page/static/js/2.12b9b962.chunk.js"
+  },
+  {
+    "revision": "120a42163d6b926ad59c",
+    "url": "/react-bank-page/static/js/main.b374bd5b.chunk.js"
+  },
+  {
+    "revision": "cc343b8c28ca487a3302",
+    "url": "/react-bank-page/static/js/runtime~main.62a95d8f.js"
+  }
+]);
